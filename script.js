54fcc86d6ebac780bdd74703f4f5c26e904b724c
HarprabhGill <harprabh.gill@ucalgary.ca>
2022-10-18 20:52:04 -0600

    new pics, delay and reset features

--- script.js
+++ script.js
@@ -12,23 +12,27 @@ $("#first").on("click", async function(event) {
     var x = event.pageX - this.offsetLeft;
 
     if(piccounter == 0){
-        y < this.height / 2 ? alert('top') : (piccounter +=1, document.getElementById("first").src = "assets/dog2.png");
+        y < this.height / 2 ? reset() : (piccounter +=1, document.getElementById("first").src = "assets/dog2.png");
     }
     else if(piccounter == 1) {
-        x < this.width / 2 ? alert('left') : (piccounter +=1, document.getElementById("first").src = "assets/dog3.png");
+        x < this.width / 2 ? reset() : (piccounter +=1, document.getElementById("first").src = "assets/dog3.png");
     }
     else if(piccounter == 2) {
-        x < this.width / 2 ? (piccounter +=1, document.getElementById("first").src = "assets/dog4.png") : alert('right');
+        x < this.width / 2 ? (piccounter +=1, document.getElementById("first").src = "assets/dog4.png") : reset();
     }
     else if(piccounter == 3) {
         document.getElementById("first").src = "assets/dog5.png";
+        $("#unltext").show();
         const result = await delay();
         unlocked(this.parentNode);
     }
-    else if(piccounter == 4) {
-        y < this.height / 2 ? alert('up') : unlocked(this.parentNode);
-    }
 });
+
+async function reset() {
+    $("#wrongtxt").show();
+    const result = await delay();
+    location.reload();
+}
 
 function unlocked(parent) {
     parent.remove();
