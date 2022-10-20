a44397a2a796182a2663e2499db3331ebb413e28
HarprabhGill <harprabh.gill@ucalgary.ca>
2022-10-20 17:05:18 -0600

    added swipe, animated check mark, and dynamic screen sizing

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function passcode(parent) {
-    parent.remove();
-    $("#origami").show();
-}
+document.getElementById('lock').addEventListener('swiped-down', function(e) {
+    console.log(e.target); // the element that was swiped
+});
 
 let piccounter = 0;
 $("#first").on("click", async function(event) {
@@ -46,3 +45,34 @@ function delay() {
       }, 3000);
     });
 }
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+                                                     
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                                                           
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if (! yDown ) {
+        return;
+    }
+
+    var yUp = evt.touches[0].clientY;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( yDiff > 0 ) {
+        $("#lock").remove();
+        $("#origami").show();
+    }                                                             
+    /* reset values */
+    yDown = null;                                             
+};
